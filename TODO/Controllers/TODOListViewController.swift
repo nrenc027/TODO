ff62cc367408bf49f9f9dd3554a30a5e3b96aef6
Ren Cummings <rcummings@miletwo.us>
2018-04-09 12:52:55 -0400

    ncoder persistent data

--- TODO/Controllers/TODOListViewController.swift
+++ TODO/Controllers/TODOListViewController.swift
@@ -11,14 +11,13 @@ import UIKit
 
 class TODOListViewController: UITableViewController {
     
-   var itemArr = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+   var itemArr = [Item]()
    let defaults = UserDefaults.standard
+   let dataPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArr = items
-        }
+        loadItems()
     }
     
     //MARK - Tableview Datasource Methods:
@@ -29,7 +28,11 @@ class TODOListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItmCell", for: indexPath)
-        cell.textLabel?.text = itemArr[indexPath.row]
+        let item = itemArr[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         return cell
     }
     
@@ -37,15 +40,8 @@ class TODOListViewController: UITableViewController {
     
     override  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-      
-        
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-
-        }
+        itemArr[indexPath.row].done = !itemArr[indexPath.row].done
+        saveItem()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -57,22 +53,42 @@ class TODOListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             //add item functionality
-            self.itemArr.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArr.append(newItem)
+            self.saveItem()
             
-            self.defaults.set(self.itemArr, forKey: "TodoListArray")
-            
-            self.tableView.reloadData()
         }
         alert.addAction(action)
         alert.addTextField { (alertTextInput) in
             alertTextInput.placeholder = "create new item"
             textField = alertTextInput
-            
-
-            
         }
         present(alert, animated: true, completion: nil)
+    }
+    func saveItem() {
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(itemArr)
+            try data.write(to: dataPath!)
+        } catch {
+            print("Error: encoding item array, \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataPath!) {
         
+            let decoder = PropertyListDecoder()
+            do {
+            itemArr = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error: Decode \(error)")
+            }
+            
+        }
+    
     }
     
 }
---
+++ TODO/Data Model/item.swift
@@ -0,0 +1,13 @@
+//
+//  item.swift
+//  TODO
+//
+//  Created by Ren Cummings on 4/9/18.
+//  Copyright © 2018 Ren Cummings. All rights reserved.
+//
+
+import Foundation
+class Item : Codable {
+    var title : String = ""
+    var done: Bool = false
+}
